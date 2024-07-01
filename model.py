454ce562ede7fbfe8928415629d0ff2b10d46f0c
Bobie Dubose <bobiedubose@Bobies-MBP.attlocal.net>
2024-06-30 20:02:30 -0500

    template for UNET++ model added

--- model.py
+++ model.py
@@ -1,1 +1,55 @@
-import torch
+# importing PyTorch Deep Learning Framework
+import torch 
+import numpy 
+import matplotlib.pyplot as plt
+import os
+
+# importing OpenCV (testing model dimensions)
+import cv2
+
+# importing for read and write of neuroimaging data 
+import nibabel
+
+class UNETpp(torch.nn.Module):
+    # UNET++ model implementation 
+
+    def __init__:
+        super.__init__()
+        
+
+    class BottleNeckBlock(torch.nn.Module):
+        def __init__:
+            super.__init__()
+
+        def forward(x):
+            return 0
+        
+
+    class EncoderBlock(torch.nn.Module):
+        def __init__:
+            super.__init__()
+
+        def forward(x):
+            return 0
+
+
+    class DecoderBlock(torch.nn.Module):
+        def __init__:
+            super.__init__()
+
+        def forward(x):
+            return 0
+
+    def forward(x):
+            return 0
+
+    
+
+    
+
+
+
+
+
+
+
